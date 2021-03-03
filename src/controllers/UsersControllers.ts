188486adb1536cd91e9414eed2aa76e46742018f
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-03-03 20:17:05 -0300

    add .gitignore

--- src/controllers/UsersControllers.ts
+++ src/controllers/UsersControllers.ts
@@ -1,8 +1,11 @@
+// eslint-disable-next-line import/no-unresolved
 import User from '@models/User';
 
 export default class UsersControllers {
+  // eslint-disable-next-line class-methods-use-this
   teste() {
     const user = new User();
+    // eslint-disable-next-line no-console
     console.log(user);
   }
 }
